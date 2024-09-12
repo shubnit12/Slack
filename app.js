80111d35a0f4087c0f3572c090fd03b2b44af69d
shubnit12 <shubnit12@gmail.com>
2024-09-12 17:17:33 +0530

    added db

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
-
+const admin = require('firebase-admin')
 const app = express()
 app.use(express.json())
+
+const serviceAccount = require('./keys/alerttoexcel-firebase-adminsdk-as4d4-cab3c2d204.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://alerttoexcel-default-rtdb.firebaseio.com/"
+})
 
 app.get('/',(req,res) =>{
  console.log(req)
@@ -9,9 +16,17 @@ app.get('/',(req,res) =>{
 
 })
 
-app.post('/',(req,res) =>{
+app.post('/',async (req,res) =>{
     console.log(req.body)
-    res.status(200).send({challange: req.body.challenge})
+    try{
+        await admin.database().ref('messsages').push(message)
+        res.status(200).send('Message Logged to DB')
+    }catch(error){
+        console.error('Error while send to DB',error)
+        res.status(500).send('Error Logging the message to DB')
+
+    }
+    
 })
 
 
